d72335e9fb9092513f82a1776f236e9563dfadda
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-16 17:48:10 +0330

    commit> Add Model (Food)

---
+++ lib/model/food_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutterblocsqflite/db/database_provider.dart';
+
+class Food {
+  int id;
+  String name;
+  String calories;
+  bool isVegetarian;
+
+  Food(this.id, this.name, this.calories, this.isVegetarian);
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{
+      DatabaseProvider.COLUMN_NAME: name,
+      DatabaseProvider.COLUMN_CALORIES: calories,
+      DatabaseProvider.COLUMN_VEGETARIAN: isVegetarian ? 1 : 0,
+    };
+    if (id != null) {
+      map[DatabaseProvider.COLUMN_ID] = id;
+    }
+    return map;
+  }
+
+  Food.fromMap(Map<String, dynamic> map) {
+    id = map[DatabaseProvider.COLUMN_ID];
+    calories = map[DatabaseProvider.COLUMN_CALORIES];
+    name = map[DatabaseProvider.COLUMN_NAME];
+    isVegetarian = map[DatabaseProvider.COLUMN_VEGETARIAN] == 1;
+  }
+}
