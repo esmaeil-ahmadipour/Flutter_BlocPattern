7cee5ae7240cbe2a7dbe30ca420f74099b59badb
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-16 20:17:46 +0330

    commit> update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterblocsqflite/bloc/food_bloc.dart';
 import 'package:flutterblocsqflite/home_page.dart';
 
-void main() {
-  runApp(HomePage());
+void main() => runApp(MainPage());
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<FoodBloc>(
+      create: (context) => FoodBloc(),
+      child: MaterialApp(
+        title: 'Sqflite Tutorial',
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
 }
