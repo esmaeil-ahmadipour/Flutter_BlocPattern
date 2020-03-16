0af9483f7fcbbc31ea201c1bf0b8d932a1cfe06f
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-16 16:37:00 +0330

    Initial commit

---
+++ android/app/src/main/kotlin/ir/ea2/flutterblocsqflite/MainActivity.kt
@@ -0,0 +1,6 @@
+package ir.ea2.flutterblocsqflite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutterblocsqflite/home_page.dart';
+
+void main() {
+  runApp(HomePage());
+}
