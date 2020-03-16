68c5c66a7df0fc8586bf8e0cfd75de77f56cf9c1
Esmaeil Ahmadipour <software8686@gmail.com>
2020-03-16 20:13:37 +0330

    commit> update HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterblocsqflite/db/database_provider.dart';
+import 'package:flutterblocsqflite/event/delete_food.dart';
+import 'package:flutterblocsqflite/event/set_foods.dart';
+import 'package:flutterblocsqflite/food_form.dart';
+import 'package:flutterblocsqflite/model/food_model.dart';
 
-class HomePage extends StatelessWidget {
+import 'bloc/food_bloc.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    DatabaseProvider.db.getFoods().then(
+      (foodList) {
+        BlocProvider.of<FoodBloc>(context).add(SetFoods(foodList));
+      },
+    );
+  }
+
+  showFoodDialog(BuildContext context, Food food, int index) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(food.name),
+        content: Text("ID ${food.id}"),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => FoodForm(food: food, foodIndex: index),
+              ),
+            ),
+            child: Text("Update"),
+          ),
+          FlatButton(
+            onPressed: () => DatabaseProvider.db.delete(food.id).then((_) {
+              BlocProvider.of<FoodBloc>(context).add(
+                DeleteFood(index),
+              );
+              Navigator.pop(context);
+            }),
+            child: Text("Delete"),
+          ),
+          FlatButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Cancel"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(),
+    return Scaffold(
+      appBar: AppBar(title: Text("Food List")),
+      body: Container(
+        child: BlocConsumer<FoodBloc, List<Food>>(
+          builder: (context, foodList) {
+            return ListView.separated(
+              itemBuilder: (BuildContext context, int index) {
+                print("Food List: $foodList");
+
+                Food food = foodList[index];
+                return ListTile(
+                    title: Text(food.name, style: TextStyle(fontSize: 30)),
+                    subtitle: Text(
+                      "Calories: ${food.calories}\ Vegetarian: ${food.isVegetarian}",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onTap: () => showFoodDialog(context, food, index));
+              },
+              itemCount: foodList.length,
+              separatorBuilder: (BuildContext context, int index) =>
+                  Divider(color: Colors.black),
+            );
+          },
+          listener: (BuildContext context, foodList) {},
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (BuildContext context) => FoodForm()),
+        ),
       ),
     );
   }
